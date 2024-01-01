35d7cbbe26377e36a04809eecc44de5ce46413c0
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2024-01-01 15:34:33 +0530

    update on api data

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const API = "d064627dfb810fd067d5be2d911909fa";
 export const getWeatherData = async (city) => {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${API}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`
     );
     const newData = await response.json();
     return newData;
